1daf50e0fb40385aeab746752f1bbf9158a4fc0e
Efi Kay <efikay@icloud.com>
2024-04-24 07:33:29 +0300

    Update zen-mode config

--- lua/efikay/plugins/zen-mode.lua
+++ lua/efikay/plugins/zen-mode.lua
@@ -8,8 +8,8 @@ return {
         options = {
           -- foldcolumn = "0", -- disable fold column
           -- signcolumn = "no", -- disable signcolumn
-          number = false, -- disable number column
-          relativenumber = false, -- disable relative numbers
+          -- number = false, -- disable number column
+          -- relativenumber = false, -- disable relative numbers
           -- cursorline = false, -- disable cursorline
           -- cursorcolumn = false, -- disable cursor column
           -- foldcolumn = "0", -- disable fold column
