f860b3cdc1e2367685f49f22bd2859c8590c1468
Efi Kay <efikay@icloud.com>
2024-04-30 13:05:04 +0300

    Add ru->en langmap. Add jk bind (ru version)

--- lua/efikay/core/keymaps.lua
+++ lua/efikay/core/keymaps.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "ол", "<ESC>", { desc = "Exit insert mode with ол(jk in russian)" })
 
 -- keymap.set("n", "<leader>-", vim.cmd.Ex, { desc = "Open Explorer" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
