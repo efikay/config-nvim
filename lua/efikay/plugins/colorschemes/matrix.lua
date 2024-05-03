5ee525bc3af1be514c6a6de840fff0e41f2f6852
Efi Kay <efikay@icloud.com>
2024-05-03 09:05:03 +0300

    Set catppuccin as active theme

--- lua/efikay/plugins/colorschemes/matrix.lua
+++ lua/efikay/plugins/colorschemes/matrix.lua
@@ -2,7 +2,7 @@ return {
   "iruzo/matrix-nvim",
   name = "matrix",
   priority = 1000,
-  enabled = true,
+  enabled = false,
   config = function()
     vim.g.matrix_disable_background = true -- true=use terminal background
 
